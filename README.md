a34ad1f4024a25e65fac4570ad10af516ac5958b
SuperGarryGamer <supergarrygamer@gmail.com>
2022-07-27 23:03:40 +0200

    add code and licenanse lmao

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# bible-pronouns
-Why does Lavern Spicer?
+<a href="http://www.wtfpl.net/"><img src="http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-4.png" width="80" height="15" alt="WTFPL" /></a>
+
+# "There are no pronouns in the Bible."
+## - Lavern Spicer, 2022
+
+This is a simple project that counts the pronouns in the American Standard Version of the Bible. This does not accurately count all pronouns, as some words may be used as either a pronoun or other type of word. Go see the code yourself; why am i explaining this?
+
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import re
+
+pronouns = [
+    "i", "me", "my", "mine", "myself",
+    "you", "your", "yours", "yourself",
+    "he", "him", "his", "himself",
+    "she", "her", "hers", "herself",
+    "they", "them", "their", "theirs", "themself",
+    "it", "its", "itself",
+    "we", "us", "our", "ours", "ourselves",
+    "yourselves", "themselves",
+    "thee", "thou", "thy", "thine", "thyself",
+    "ye"
+]
+
+with open("asv.txt", "r") as f:
+    f_data = f.read()
+    pattern = re.compile(f"\\b({'|'.join(pronouns)})\\b")
+
+    matches = re.findall(pattern, f_data)
+    
+    print(f"The ASV Bible countains at least {len(matches)} pronouns. \nAt least{len(matches) / len(f_data.split()) * 100}% of words in the ASV Bible are pronouns.")
